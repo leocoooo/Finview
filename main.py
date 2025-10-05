a1b23ad29daa56edd8a1ac2c893ae2d0e5a80199
Colin <leocolin7002@gmail.com>
2025-10-05 16:37:30 +0200

    amélioration du dashbaord sur les actifs

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ from portfolio_package.wealth_management_functions import (
 )
 
 from portfolio_package.create_demo_portfolio import (create_demo_portfolio_bis, 
-                                                     create_demo_portfolio)
+                                                     create_demo_portfolio,
+                                                     create_demo_portfolio_3)
 
 from portfolio_package.save_load_ptf_functions import save_portfolio, load_portfolio
 
@@ -62,7 +63,7 @@ def main():
         portfolio=st.session_state.portfolio,
         save_portfolio_func=save_portfolio,
         Portfolio=Portfolio,
-        create_demo_portfolio_func=create_demo_portfolio_bis,
+        create_demo_portfolio_func=create_demo_portfolio_3,
         generate_pdf_func=generate_portfolio_pdf
     )
 
